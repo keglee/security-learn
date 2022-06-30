9f0cb9227b7bd32d5fb0801897ecfcb6623d956e
lijie <lijie@dataeye.com>
2022-06-30 14:31:36 +0800

    SessionManagement

--- session-management-sample/src/main/java/com/iverson/learn/security/config/SecurityConfig.java
+++ session-management-sample/src/main/java/com/iverson/learn/security/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.iverson.learn.security.config;
 
 import com.iverson.learn.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.session.SessionInformationExpiredEvent;
 import org.springframework.security.web.session.SessionInformationExpiredStrategy;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +22,9 @@ import java.util.Arrays;
  **/
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private FindByIndexNameSessionRepository sessionRepository;
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -43,11 +50,16 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                     }
                 })
                 // 阻止用户在达到最大会话数时进行身份验证
-                .maxSessionsPreventsLogin(true);
+                .maxSessionsPreventsLogin(true).sessionRegistry(sessionRegistry());
     }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(new UserService(Arrays.asList("admin", "test")));
+    }
+    
+    @Bean
+    public SpringSessionBackedSessionRegistry sessionRegistry() {
+        return new SpringSessionBackedSessionRegistry(sessionRepository);
     }
 }
--- session-management-sample/src/main/java/com/iverson/learn/security/controller/IndexController.java
+++ session-management-sample/src/main/java/com/iverson/learn/security/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 /**
  *
  **/
@@ -12,7 +14,7 @@ public class IndexController {
     @Value("${server.port}")
     private Integer port;
     @RequestMapping("/index")
-    public String index() {
-        return "index:" + port;
+    public String index(HttpSession session) {
+        return "index:" + port + ":" + session.getId();
     }
 }
